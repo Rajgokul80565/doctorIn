ab20603555e9dda992daa9a120efbb5df6f1f430
Rajgokul80565 <74341639+Rajgokul80565@users.noreply.github.com>
2023-06-05 21:19:43 +0530

    db connect and added jwt auth

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,70 @@
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+import asyncHandler from "express-async-handler";
+
 // @desc Authenticate User
 // @route POST - /api/user/auth
 // @access public
-const authUser = (req, res) => {
+const authUser = asyncHandler( async (req, res) => {
 
     res.status(201).json({message:"Auth User"});
-}
+});
+
+// @desc Registrer User
+// @route POST - /api/users
+// @access public
+const registerUser = asyncHandler (async (req, res) => {
+    let {name, email, password} = req.body;
+
+    console.log('name:', name, email, password);
 
 
-export { authUser };
+    let userExist = await User.findOne({email});
+
+    if(userExist) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    let user = await User.create({name,email,password});
+
+    if(user){
+        generateToken(res,user._id);
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+        })
+    }else{
+        res.status(400);
+        throw new Error('invalid user data')
+    }
+});
+
+// @desc  Logout user
+// @route POST - /api/user/logout
+// @access public
+const loginOutUser =  asyncHandler( async (req, res) => {
+
+    res.status(201).json({message:"loginOutUser"});
+});
+
+// @desc  get user profile
+// @route GET - /api/user/profile
+// @access private
+const getUserProfile = asyncHandler( async(req, res) => {
+
+    res.status(201).json({message:"getUserProfile"});
+});
+
+// @desc  update user profile
+// @route PUT - /api/user/profile
+// @access private
+const updateUserProfile = asyncHandler ( async (req, res) => {
+
+    res.status(201).json({message:"updateUserProfile"});
+});
+
+
+
+export { authUser, registerUser, loginOutUser, getUserProfile , updateUserProfile};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { authUser } from "../controllers/userControllers.js";
+import { authUser, registerUser, loginOutUser,  getUserProfile, updateUserProfile } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", loginOutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+// router.post("/profile", getUserProfile);
+// router.get("/profile", updateUserProfile);
 
 
 export default router
