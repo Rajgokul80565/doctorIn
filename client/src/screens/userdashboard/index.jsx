1d4a7534201c40b0e660622011fe77782be004ab
Rajgokul80565 <74341639+Rajgokul80565@users.noreply.github.com>
2024-01-02 21:56:08 +0530

    Sidebar completed

--- client/src/screens/userdashboard/index.jsx
+++ client/src/screens/userdashboard/index.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../userdashboard/userdashboardStyles.css";
 import { IoMdHome } from "react-icons/io";
 import { FaBloggerB } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 
 function UserDashboard() {
+
+  const [openSide, setOpenSide] = useState(false);
+
   return (
-    <div className='main'>
+    <div className={`main ${openSide ? "active" : "inactive"}`}>
         <div className="sidebar_main">
         <div className="sidebar_main_item">
         <IoMdHome className='sidebar_icon'/>
-          <h6>Home</h6>
+        { openSide && <h6>Home</h6>}
+          
         </div>
        
         <div className="sidebar_main_item">
         <FaBloggerB />
-          <h6>Blogs</h6>
+        { openSide && <h6>Blogs</h6>}
+       
         </div>
+        {openSide ? (
+          <div onClick={() => setOpenSide(!openSide)} className="sidebar_main_item sidebar_btn">
+          <FaArrowLeft/>
         </div>
+        ) : (
+          <div onClick={() => setOpenSide(!openSide)} className="sidebar_main_item sidebar_btn">
+        <FaArrowRight />
+        </div>
+         
+        )}
+       
+
+        </div>
+        
         <div className="main_content">
           maincontent
         </div>
