3d34e7827a96807db8f4557ae0639d6100729b52
Rajgokul80565 <74341639+Rajgokul80565@users.noreply.github.com>
2024-01-08 22:42:36 +0530

    welcome section

--- client/src/screens/userdashboard/index.jsx
+++ client/src/screens/userdashboard/index.jsx
@@ -5,11 +5,25 @@ import { FaBloggerB } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 import {ToolTip, DoctorsCard} from "../../components";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 
 function UserDashboard() {
 
   const [openSide, setOpenSide] = useState(false);
+
+  const sliderLeft = () => {
+    debugger;
+    let sliderback = document.getElementById("card_slider");
+    sliderback.scrollLeft = sliderback.scrollLeft - 550;
+  }
+
+  const sliderRight = () => {
+    debugger;
+    let sliderForward = document.getElementById("card_slider");
+    sliderForward.scrollLeft = sliderForward.scrollLeft + 550;
+  }
 
   return (
     <div className={`main ${openSide ? "active" : "inactive"}`}>
@@ -46,6 +60,7 @@ function UserDashboard() {
             <div className="left_Main">LeftMain</div>
             <div className="right_Main">
             <div className="doctor_list_container_1">
+            <IoIosArrowBack onClick={sliderLeft} className="card_slider_icon"/>
             <div id='card_slider'>
             <div className="card_slider_item">
             <DoctorsCard />
@@ -59,8 +74,23 @@ function UserDashboard() {
             <div className="card_slider_item">
             <DoctorsCard />
             </div>
+            <div className="card_slider_item">
+            <DoctorsCard />
             </div>
-           
+            <div className="card_slider_item">
+            <DoctorsCard />
+            </div>
+            <div className="card_slider_item">
+            <DoctorsCard />
+            </div>
+            <div className="card_slider_item">
+            <DoctorsCard />
+            </div>
+            <div className="card_slider_item">
+            <DoctorsCard />
+            </div>
+            </div>
+            <IoIosArrowForward onClick={sliderRight} className="card_slider_icon"/>
             </div>
             <div className="doctor_list_container_2">
             
